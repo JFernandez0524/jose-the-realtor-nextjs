29c1d4bdeb886e6eeb8bc8f048d5fdc9ba11582f
Jose Fernandez <njphonefix@gmail.com>
2024-03-24 09:39:50 -0400

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,10 @@ import Header from './Header';
 
 export default function HomePage() {
   return (
-    <div>
-      <Header />
-    </div>
+    <section id="hero-section" class="bg-hero-pattern bg-cover h-[500px] max-h-[500px] bg-center bg-repeat">
+        <div class="container mx-auto">
+          <h1>Jose FErnandez REal Estate</h1>
+        </div>
+      </section>
   );
 }
